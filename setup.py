0314350f7440f8e1a3efe1fb62c2f4138ca1529e
sujata <sujata16k@gmail.com>
2022-10-16 12:43:57 +0530

    first update

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages,setup
+
+setup(
+    name ="sensor",
+    version="0.0.1",
+    author="sujata",
+    author_email="sujata16k@gmail.com",
+    packages=find_packages()
+
+
+)
